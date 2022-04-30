230e016595f1c75b1f83aa7bf155ee698cde3b50
brett hockridge <bretthockridge98@gmail.com>
2022-04-30 13:56:38 -0700

    added api links, new background picture, and started JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 var apiKey = 'ff7f3c0db4b8ea06b5433dfc17d1ec10';
+
+var mainDisplayCity = document.querySelector("#mainCity");
+var mainTemperature = document.querySelector("#mainTemp");
+var mainWindSpeed = document.querySelector("#mainWind");
+var mainHumidity = document.querySelector("#mainHumid");
+var mainUV = document.querySelector("#UVIndex");
