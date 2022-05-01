57cbcd202ed2420da8c2f9be7d704c50653e826a
brett hockridge <bretthockridge98@gmail.com>
2022-05-01 12:11:04 -0700

    added js variables and added custom hover features

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ var mainTemperature = document.querySelector("#mainTemp");
 var mainWindSpeed = document.querySelector("#mainWind");
 var mainHumidity = document.querySelector("#mainHumid");
 var mainUV = document.querySelector("#UVIndex");
+
+var forecastDay1 = document.querySelector(".day1");
+var day1Temperature = document.querySelector(".temp1");
+var day1WindSpeed = document.querySelector(".wind1");
+var day1Humidity = document.querySelector(".humid1");
+
+var forecastDay2 = document.querySelector(".day2");
+var day2Temperature = document.querySelector(".temp2");
+var day2WindSpeed = document.querySelector(".wind2");
+var day2Humidity = document.querySelector(".humid2");
+
+var forecastDay3 = document.querySelector(".day3");
+var day3Temperature = document.querySelector(".temp3");
+var day3WindSpeed = document.querySelector(".wind3");
+var day3Humidity = document.querySelector(".humid3");
+
+var forecastDay4 = document.querySelector(".day4");
+var day4Temperature = document.querySelector(".temp4");
+var day4WindSpeed = document.querySelector(".wind4");
+var day4Humidity = document.querySelector(".humid4");
+
+var forecastDay5 = document.querySelector(".day5");
+var day5Temperature = document.querySelector(".temp5");
+var day5WindSpeed = document.querySelector(".wind5");
+var day5Humidity = document.querySelector(".humid5");
